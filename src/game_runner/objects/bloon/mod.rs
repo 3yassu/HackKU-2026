ab5464ea8d0662e1fa47077ef8e1330cf3415a42
3yassu <68251430+3yassu@users.noreply.github.com>
2026-04-18 14:57:57 -0500

    start actually implementing these traits

--- src/game_runner/objects/bloon/mod.rs
+++ src/game_runner/objects/bloon/mod.rs
@@ -5,10 +5,11 @@ struct map_thingie {
 	nah: u32,
 }
 
-pub trait Enemy {
-	fn new(hp: u16) -> Self;
+pub trait Enemy<'a> {
+	fn new(hp: u16, location: (f32, f32), map: &'a map_thingie) -> Self;
+/*
 	fn draw(&mut self);
-	fn enemy_set_path(&mut self, map: &map_thingie);
 	fn move_enemy(&mut self);
 	fn damage(&mut self, dmg_amt: u32);
+*/
 }
--- src/game_runner/objects/bloon/regular.rs
+++ src/game_runner/objects/bloon/regular.rs
@@ -1,13 +1,15 @@
-struct Regular {
+use super::*;
+
+struct Regular<'a>{
 	hp: u16,
-	location: f32
-	
+	location: (f32, f32),
+	map: &'a map_thingie,
 }
 
 
-impl Default for Regular {
-	fn default() -> Self {
-		Self{hp: 1}
-	}
+impl<'a> Enemy<'a> for Regular<'a> {
+	fn new(hp: u16, location: (f32, f32), map: &'a map_thingie) -> Self {
+		Self{hp: 1, location, map} 
+	} 
 }
 
